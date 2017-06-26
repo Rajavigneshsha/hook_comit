2d7cef51d6afc363afd9ce1cb32fc6691375b468
Rajavignesh <uvaraj.seerangan@treselle.com>
2017-06-26 11:04:33 +0530

    third commit

--- HookCommit.java
+++ HookCommit.java
@@ -3,5 +3,6 @@ public class HookCommit {
 	public static void main(String arg[]) {
 		System.out.println("hooks commit started ........");
 		System.out.println("commit made now .............");
+		System.out.println("now is it working");
 	}
 }
